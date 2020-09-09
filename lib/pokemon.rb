201e009353f805419bcc3008cac57e71237bb415
Harley Hanna <harleyhannawrit@gmail.com>
2020-09-09 15:37:56 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,14 +19,14 @@ class Pokemon
       db.execute(sql, name, type)
   end
   
-  def self.find(id, db)
+  def self.find(id, @db)
     sql = <<-SQL
       SELECT *
       FROM pokemon
       WHERE id = ?
     SQL
     
-    db.execute(sql, id).map do |row|
+    db.execute(sql, @id)
   end
   
   
